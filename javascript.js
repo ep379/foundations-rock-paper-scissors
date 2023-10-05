b73a14ada1067072338b265e2f3a212e6127465c
Emmanuel <ep379@cornell.edu>
2023-10-05 19:41:23 -0400

    added the game function that simulates 5 rounds of rock paper scissors and outputs it to the console.

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ function getComputerChoice () {
 }
 function roundOfRPS () {
     let computerSelection = getComputerChoice();
-    let playerSelection = toUpperCase(prompt("Time for a round of Rock-Paper-Scissors. Please enter your choice."));
+    let playerSelection = prompt("Time for a round of Rock-Paper-Scissors. Please enter your choice.").toUpperCase();
     if (computerSelection == playerSelection) {
         return "It's a tie!";
     }
@@ -24,7 +24,7 @@ function roundOfRPS () {
     }
 
     else if (computerSelection == "SCISSORS" && playerSelection == "PAPER") {
-        return "You lose. Scissors beats Paper";
+        return "You lose! Scissors beats Paper";
     }
 
     else if (computerSelection == "ROCK" && playerSelection == "SCISSORS") {
@@ -33,6 +33,28 @@ function roundOfRPS () {
     else if (computerSelection == "ROCK" && playerSelection == "PAPER") {
         return "You win! Paper beats Rock";
     }
+    else {
+        return "No Contest. Next time enter Rock, Paper, or Scissors."
+    }
+}
+function game() {
+    let wins = 0;
+    let losses = 0;
+    let ties = 0;
+    for (let i = 0; i < 5; i++)
+    {
+        let result = roundOfRPS();
+        if (result.includes("You lose!") == true) {
+            losses++;
+        }
+        else if (result.includes("You win!") == true) {
+            wins++;
+        }
+        else if (result.includes("It's a tie!") == true) {
+            ties++;
+        }
+    }
+    console.log("You've won " + wins + " times, lost " + losses + " times, and tied " + ties + " times.")
 }
 
-console.log(roundOfRPS());
+game();
