5e13e7b4b0222927b7ec9e7cd7cd5eb25c27faf1
Emmanuel <ep379@cornell.edu>
2023-10-10 18:58:29 -0400

    Updated the version now using a DOM and a simple UI

--- javascript.js
+++ javascript.js
@@ -4,57 +4,111 @@ function getComputerChoice () {
     console.log("The Computer has chosen: " + computersChoice);
     return computersChoice
 }
-function roundOfRPS () {
+function playRound (playersPick) {
+    playersPick = playersPick.currentTarget.textContent;
+    let outputText = "";
     let computerSelection = getComputerChoice();
-    let playerSelection = prompt("Time for a round of Rock-Paper-Scissors. Please enter your choice.").toUpperCase();
-    if (computerSelection == playerSelection) {
-        return "It's a tie!";
+    if (computerSelection == playersPick) {
+        outputText += "It's a tie!";
     }
 
-    else if (computerSelection == "PAPER" && playerSelection == "ROCK") {
-        return "You lose! Paper Beats Rock";
+    else if (computerSelection == "PAPER" && playersPick == "ROCK") {
+        outputText += "You lose! Paper Beats Rock";
     }
 
-    else if (computerSelection == "PAPER" && playerSelection == "SCISSORS") {
-        return "You win! Scissors beats Paper";
+    else if (computerSelection == "PAPER" && playersPick == "SCISSORS") {
+        outputText += "You win! Scissors beats Paper";
     }
 
-    else if (computerSelection == "SCISSORS" && playerSelection == "ROCK"){
-        return "You win! Rock beats Scissors";
+    else if (computerSelection == "SCISSORS" && playersPick == "ROCK"){
+        outputText += "You win! Rock beats Scissors";
     }
 
-    else if (computerSelection == "SCISSORS" && playerSelection == "PAPER") {
-        return "You lose! Scissors beats Paper";
+    else if (computerSelection == "SCISSORS" && playersPick == "PAPER") {
+        outputText += "You lose! Scissors beats Paper";
     }
 
-    else if (computerSelection == "ROCK" && playerSelection == "SCISSORS") {
-        return "You lose! Rock beats Scissors";
+    else if (computerSelection == "ROCK" && playersPick == "SCISSORS") {
+        outputText += "You lose! Rock beats Scissors";
     }
-    else if (computerSelection == "ROCK" && playerSelection == "PAPER") {
-        return "You win! Paper beats Rock";
+    else if (computerSelection == "ROCK" && playersPick == "PAPER") {
+        outputText += "You win! Paper beats Rock";
     }
     else {
-        return "No Contest. Next time enter Rock, Paper, or Scissors."
+        outputText += "No Contest. Next time enter Rock, Paper, or Scissors.";
+    }
+    document.getElementById("outputResponse").textContent = outputText;
+    if (outputText.includes("You lose!") == true) {
+        let computersUpdatedScore = parseInt(document.getElementById('computerScore').textContent) + 1;
+        document.getElementById('computerScore').textContent = computersUpdatedScore;
+        if (computersUpdatedScore == 5) {
+            alert("RIP: You've lost this match");
+        }
+    }
+    else if (outputText.includes("You win!") == true) {
+        let yourUpdatedScore = parseInt(document.getElementById('yourScore').textContent) + 1;
+        document.getElementById('yourScore').textContent = yourUpdatedScore;
+        if (yourUpdatedScore == 5) {
+            alert("Congrats! You've won the match")
+        }
+
+    }
+    else if (outputText.includes("It's a tie!")) {
+        let tiedUpdatedScore = parseInt(document.getElementById('tiedScore').textContent) + 1;
+        document.getElementById('tiedScore').textContent = tiedUpdatedScore;
     }
 }
 function game() {
     let wins = 0;
     let losses = 0;
     let ties = 0;
-    for (let i = 0; i < 5; i++)
-    {
-        let result = roundOfRPS();
-        if (result.includes("You lose!") == true) {
-            losses++;
-        }
-        else if (result.includes("You win!") == true) {
-            wins++;
-        }
-        else if (result.includes("It's a tie!") == true) {
-            ties++;
-        }
+    while (wins < 5 && losses < 5) {
+        let result = playRound
     }
-    console.log("You've won " + wins + " times, lost " + losses + " times, and tied " + ties + " times.")
+    div.textContent("You've won " + wins + " times, lost " + losses + " times, and tied " + ties + " times.");
 }
+const div = document.createElement('div');
+div.setAttribute('id', 'outputResponse');
 
-game();
+const scores = document.createElement('div');
+const yourScore = document.createElement('div');
+yourScore.setAttribute('id','yourScore')
+yourScore.textContent = 0;
+
+const computerScore = document.createElement('div');
+computerScore.setAttribute('id','computerScore');
+computerScore.textContent = 0;
+
+const tiedScore = document.createElement('div');
+tiedScore.setAttribute('id', 'tiedScore');
+tiedScore.textContent = 0;
+
+const rock = document.createElement('button');
+rock.setAttribute('id','rock');
+rock.textContent = 'ROCK';
+rock.addEventListener('click', playRound);
+
+const paper = document.createElement('button');
+paper.setAttribute('id','paper');
+paper.textContent = 'PAPER';
+paper.addEventListener('click', playRound);
+
+const scissors = document.createElement('button');
+scissors.setAttribute('id','scissors');
+scissors.textContent = 'SCISSORS';
+scissors.addEventListener('click', playRound);
+
+
+
+const body = document.querySelector('body');
+body.appendChild(rock);
+body.appendChild(paper);
+body.appendChild(scissors);
+body.appendChild(div);
+body.appendChild(scores);
+scores.appendChild(yourScore);
+scores.appendChild(computerScore);
+scores.appendChild(tiedScore);
+
+// to do: inside the playround function, call another function that will keep sccores
+// and output what it is we need and whatnot.
